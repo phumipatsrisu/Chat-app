c61e80aa0de1563aeff4dd56a24bbd1f5900474a
Phumipat <s68122250008@ssru.ac.th>
2026-09-07 17:08:53 +0700

    feat: add database connection and user model with schema

---
+++ chat-app/server/src/models/User.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+
+export default mongoose.model<IUser>("User", UserSchema);
